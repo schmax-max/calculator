bec6355325365eefe94ab5ca44e93583b6fb41bb
aschmach <anders.schmachtenberg@gmail.com>
2020-05-09 12:06:17 +1000

    proper error message on loading config variables

--- config/connection.js
+++ config/connection.js
@@ -18,14 +18,15 @@ if (env === "local") {
   require("dotenv").config();
 }
 
-
 const _DB_USERNAME = process.env._DB_USERNAME || "micro";
 const _DB_PASSWORD = process.env._DB_PASSWORD || "password";
 const _DB_HOSTS = process.env._DB_HOSTS || "localhost:27017";
 const _DB_DATABASE = process.env._DB_DATABASE || "micro-prod";
 const _DB_OPTIONS = process.env._DB_OPTIONS || "";
 
-if ()
+if (env !== "local" && _DB_USERNAME === "micro") {
+  console.log("not loaded config variables correctly");
+}
 
 let connectionString = `mongodb://${_DB_USERNAME}:${_DB_PASSWORD}@${_DB_HOSTS}/${_DB_DATABASE}${_DB_OPTIONS}`;
 
