b0c952d6fec52484d7c992fae5fdf30386a2f517
aschmach <anders.schmachtenberg@gmail.com>
2020-05-09 11:40:02 +1000

    un otra vez

--- config/connection.js
+++ config/connection.js
@@ -13,16 +13,19 @@ if (env === "local") {
     require("dotenv").config({ path: "./.envProd" });
   } else if (ensure_qa_env) {
     require("dotenv").config({ path: "./.envQa" });
-  } else {
-    require("dotenv").config();
   }
+} else {
+  require("dotenv").config();
 }
+
 
 const _DB_USERNAME = process.env._DB_USERNAME || "micro";
 const _DB_PASSWORD = process.env._DB_PASSWORD || "password";
 const _DB_HOSTS = process.env._DB_HOSTS || "localhost:27017";
 const _DB_DATABASE = process.env._DB_DATABASE || "micro-prod";
 const _DB_OPTIONS = process.env._DB_OPTIONS || "";
+
+if ()
 
 let connectionString = `mongodb://${_DB_USERNAME}:${_DB_PASSWORD}@${_DB_HOSTS}/${_DB_DATABASE}${_DB_OPTIONS}`;
 
